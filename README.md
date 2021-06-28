adfcfe4751111ced1d3341dff3af9a659cfae3b7
Harris Hudson <50556837+harrishudson@users.noreply.github.com>
2021-06-28 14:27:25 +1000

    Version 3.1.0

--- README.md
+++ README.md
@@ -7,8 +7,57 @@ Visual Field Examples: [https://visualfield.org/](https://visualfield.org/).
 ## Requirements
 Requires a HTML5 and WebSQL enabled browser.
 
-## Basic Usage
-Load vf.html in your browser in a file:// or http(s):// protocol context.
+## Application Usage
+Visual Field is intended to be able to be used in any of the following ways;
+
+- Online Web Application
+- Offline Web Application
+- As a stand alone html file
+- As an Installed Progressive Web Application
+
+### Online Web Application
+For an online web application, just install the vf.html file in your 
+intended web server directory.  This will allow Visual Field on your 
+web server to be used in an online way.
+
+### Offline Web Application
+For an offline web application, you will need to install both 
+vf.html and the vf_service_worker.js files in your intended web 
+server directory.  This will allow Visual Field to be loaded in a 
+browser even if your users do not have an internet connection 
+(after initial load).  Both the vf.hml and vf_service_worker.js 
+will need to reside in the same directory.
+
+### As a stand alone html file
+Visual Field can function as a stand alone html file just by loading the 
+vf.html file in your browser using a file:// protocol context.  That is,
+no web server is necessary to use the full functions of Visual Field.  This 
+usage option may be useful if you are unable to install Visual Field on 
+a web server or unable to install it as a Progressive Web Application.
+
+### As an Installed Progressive Web Application
+Progressive Web Applications are intended to be able to be installed on 
+your device by using HTML component "Add to Home Screen" (A2HS).  As of 
+version 3.1.0, Visual Field is typically able to be installed on your device 
+by a simple click on the "Install" option under the "Help" tab.  This 
+enables a quick, simple, non-technical install of Visual Field on to your 
+users device.  In order to support Visual Field as a Progressive Web 
+Application, you will need to place vf.html, vf_service_worker.js,
+vf.webmanifest and the favicon.svg all 4 files on to your web server in 
+the same directory.  Should all these files be installed on your web server, 
+and your web server can serve these over https, then end users should be 
+able to successfully invoke the "Install" option to add Visual Field to their 
+device as an installed Progressive Web Application.  Ie, a simple, 
+non-technical, install on their device.
+
+## Unit Test
+A script test/vf_unit_test.js is provided in the test subdirectory.  Please
+refer to instructions within this file as how to conduct some unit tests
+of some built-in Custom Functions within Visual Field.  This requires temporarily
+editing the vf.html file and adding a script reference to the 'test/vf_unit_test.js"
+javascript file.  The test/vf_unit_test.js file does not need to be deployed
+with your normal deployment of Visual Filed.  It is provided here for testing
+and diagnosing possible issues with Custom Functions. 
 
 ## Dependencies
 - Turf
@@ -29,3 +78,11 @@ Load vf.html in your browser in a file:// or http(s):// protocol context.
 
 ## Author 
 Harris Hudson
+
+## Contributing
+Pull Requests are not currently being accepted.  If you would like to 
+request a change, please raise an issue.
+
+## Donate
+Donation and Sponsorship opportunities are available.  Please contact the
+author directly.
--- vf_service_worker.js
+++ vf_service_worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var cacheVersion = '3.0.1';
+var cacheVersion = 'vf3.1.0_version_001';
 var currentCache = {
   offline: 'offline-cache' + cacheVersion
 };
@@ -8,22 +8,22 @@ var currentCache = {
 const offlineUrl = 'vf.html';
 const offlineUrls = [
  'vf.html',
- 'https://npmcdn.com/@turf/turf@5.1.6/turf.min.js', 
+ 'favicon.svg',
+ 'vf.webmanifest',
+ 'https://unpkg.com/@turf/turf@6.4.0/turf.min.js',
  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js',
- 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
- 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
- 'https://cdn.jsdelivr.net/npm/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js', 
- 'https://cdn.jsdelivr.net/npm/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css', 
- 'https://cdn.jsdelivr.net/npm/leaflet.markercluster@1.4.1/dist/MarkerCluster.css', 
- 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js', 
- 'https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.css', 
- 'https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js',
- 'https://cdn.jsdelivr.net/npm/exifreader@3.12.3/dist/exif-reader.js', 
- 'https://unpkg.com/vis-network@8.4.0/dist/vis-network.min.js', 
+ 'https://cdn.jsdelivr.net/npm/leaflet.markercluster@1.5.0/dist/leaflet.markercluster.js',
+ 'https://cdn.jsdelivr.net/npm/leaflet.markercluster@1.5.0/dist/MarkerCluster.Default.css',
+ 'https://cdn.jsdelivr.net/npm/leaflet.markercluster@1.5.0/dist/MarkerCluster.css',
+ 'https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.css',
+ 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js',
+ 'https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js',
+ 'https://cdn.jsdelivr.net/npm/exifreader@3.16.0/dist/exif-reader.js',
+ 'https://unpkg.com/vis-network@9.0.4/dist/vis-network.min.js',
  'https://cdnjs.cloudflare.com/ajax/libs/leaflet.heat/0.2.0/leaflet-heat.js',
- 'https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.6.2/proj4.js',
- 'https://cdn.jsdelivr.net/npm/jquery-csv@1.0.11/src/jquery.csv.min.js', 
+ 'https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.7.2/proj4.js',
+ 'https://cdn.jsdelivr.net/npm/jquery-csv@1.0.21/src/jquery.csv.js',
  'https://cdn.jsdelivr.net/npm/stemmer@1.0.5/index.js', 
  'https://cdn.jsdelivr.net/npm/double-metaphone@1.0.5/index.js', 
  'https://cdn.jsdelivr.net/npm/wellknown@0.5.0/index.js', 
@@ -42,30 +42,30 @@ const offlineUrls = [
 ];
 
 this.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll(offlineUrls); 
-    })
-  );
+ event.waitUntil(
+  caches.open(currentCache.offline).then(function(cache) {
+    return cache.addAll(offlineUrls); 
+  })
+ );
 });
 
 this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (event.request.mode==='navigate'||(event.request.method==='GET'&&event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl,{ignoreSearch:true});
-          })
-    );
-  }
-  else{
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request,{ignoreSearch:true})
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
+ // request.mode = navigate isn't supported in all browsers
+ // so include a check for Accept: text/html header.
+ if (event.request.mode==='navigate'||(event.request.method==='GET'&&event.request.headers.get('accept').includes('text/html'))) {
+  event.respondWith(
+  fetch(event.request.url).catch(error => {
+   // Return the offline page
+   return caches.match(offlineUrl,{ignoreSearch:true});
+   })
+  );
+ }
+ else{
+  // Respond with everything else if we can
+  event.respondWith(caches.match(event.request,{ignoreSearch:true})
+   .then(function (response) {
+    return response || fetch(event.request);
+   })
+  );
+ }
 });
